d443d111f3f54c792b28bb16b9ec05528d846710
Muhammad <muhammad.magdy50@gmail.com>
2019-02-06 21:59:34 +0200

    ++Upload Album_logo

--- music/models.py
+++ music/models.py
@@ -8,7 +8,7 @@ class Album(models.Model):
     artist = models.CharField(max_length=255)
     album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
-    album_logo = models.CharField(max_length=500)
+    album_logo = models.FileField()
 
     def get_absolute_url(self):
         return reverse('music:detail', kwargs={'pk': self.pk})
--- website/urls.py
+++ website/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path(r'admin/', admin.site.urls),
     path(r'music/', include('music.urls')),
 
 ]
+
+if settings.DEBUG :
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
